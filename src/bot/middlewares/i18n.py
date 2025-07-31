051831c20175c676b12799822ac92d33a17eaeba
ABC Developers <info@abclegacyllc.com>
2025-07-31 14:32:18 -0400

    Update i18n.py

--- src/bot/middlewares/i18n.py
+++ src/bot/middlewares/i18n.py
@@ -1,9 +1,9 @@
 from aiogram_i18n import I18nMiddleware
 from aiogram_i18n.cores import FluentRuntimeCore
-from src.bot.config import DEFAULT_LOCALE
+# Import the single settings object
+from src.bot.config import settings
 
 i18n_middleware = I18nMiddleware(
     core=FluentRuntimeCore(path="src/bot/locales/{locale}"),
-    default_locale=DEFAULT_LOCALE
+    default_locale=settings.DEFAULT_LOCALE
 )
-
