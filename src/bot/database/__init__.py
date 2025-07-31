b4b96f237423cc1d447b627cb22115500e37b8e5
ABC Developers <info@abclegacyllc.com>
2025-07-31 04:55:43 -0400

    Update __init__.py

--- src/bot/database/__init__.py
+++ src/bot/database/__init__.py
@@ -1,1 +1,6 @@
+import asyncpg
+from src.bot.config import DATABASE_URL
 
+async def create_pool():
+    # Bu funksiya avval repository.py da edi, endi shu yerda bo'lishi kerak
+    return await asyncpg.create_pool(DATABASE_URL)
