ad5fdc4ab9a3d3ce2541db083d57c1ccbe2f0f29
ABC Developers <info@abclegacyllc.com>
2025-08-03 03:41:08 -0400

    Update __init__.py

--- src/bot/database/repositories/__init__.py
+++ src/bot/database/repositories/__init__.py
@@ -2,10 +2,12 @@ from .user_repository import UserRepository
 from .channel_repository import ChannelRepository
 from .scheduler_repository import SchedulerRepository
 from .analytics_repository import AnalyticsRepository
+from .plan_repository import PlanRepository # <-- ADDED
 
 __all__ = [
     "UserRepository",
     "ChannelRepository",
     "SchedulerRepository",
     "AnalyticsRepository",
+    "PlanRepository", # <-- ADDED
 ]
