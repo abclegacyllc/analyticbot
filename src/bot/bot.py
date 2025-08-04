68a8f867a94e794add28314992aa472c7ee5bdbe
ABC Developers <info@abclegacyllc.com>
2025-08-04 05:51:13 -0400

    Update bot.py

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,12 +1,13 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+
+# Import the single settings object
 from .config import settings
-from src.bot.middlewares.i18n import i18n_middleware
 
-# Bot and Dispatcher instances are created here, but storage is configured later.
-bot = Bot(token=settings.BOT_TOKEN.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher()
-
-# Register I18nMiddleware
-i18n_middleware.setup(dispatcher=dp)
+# Only the Bot instance is created here.
+# The Dispatcher will be created in run_bot.py
+bot = Bot(
+    token=settings.BOT_TOKEN.get_secret_value(), 
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
