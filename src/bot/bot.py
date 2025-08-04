09368ade1fafe9c14ad33f45a89addcf24b7d1f4
ABC Developers <info@abclegacyllc.com>
2025-08-04 05:39:14 -0400

    Update bot.py

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,21 +1,14 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
-from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.client.default import DefaultBotProperties
-import redis.asyncio as redis
 
-# Import the single settings object instead of individual variables
+# Import the single settings object
 from .config import settings
 from src.bot.middlewares.i18n import i18n_middleware
 
-# Redis connection
-redis_conn = redis.from_url(settings.REDIS_URL.unicode_string())
-storage = RedisStorage(redis=redis_conn)
-
-# Bot and Dispatcher instances
-# Use .get_secret_value() to access the actual token from SecretStr
+# Bot and Dispatcher instances are created here, but storage is configured later.
 bot = Bot(token=settings.BOT_TOKEN.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(storage=storage)
+dp = Dispatcher()
 
 # Register I18nMiddleware
 i18n_middleware.setup(dispatcher=dp)
