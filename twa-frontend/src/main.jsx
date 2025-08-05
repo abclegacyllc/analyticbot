811fb7b7eaad98807524445f3d0777002920e88a
ABC Developers <info@abclegacyllc.com>
2025-08-05 04:57:49 -0400

    Update main.jsx

--- twa-frontend/src/main.jsx
+++ twa-frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme.js';
 
-// AppProvider endi kerak emas, chunki Zustand global holatni o'zi boshqaradi.
-// React.StrictMode'ni ham olib tashladik, chunki u TWA bilan muammo keltirib chiqarayotgan edi.
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
 )
