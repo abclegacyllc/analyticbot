048628715cf45be35f9b7063712a7d3d5a1fbefb
ABC Developers <info@abclegacyllc.com>
2025-08-05 02:36:13 -0400

    Update main.jsx

--- twa-frontend/src/main.jsx
+++ twa-frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { AppProvider } from './context/AppContext'; // Yaratgan provayderimizni import qilamiz
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppProvider> {/* Butun ilovani provayder bilan o'raymiz */}
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
 )
