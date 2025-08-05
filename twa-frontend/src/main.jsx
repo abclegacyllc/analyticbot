8e7a63610692b0c2415974e8fe789c605dfab988
ABC Developers <info@abclegacyllc.com>
2025-08-05 03:19:54 -0400

    Update main.jsx

--- twa-frontend/src/main.jsx
+++ twa-frontend/src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AppProvider } from './context/AppContext'; // Yaratgan provayderimizni import qilamiz
 
+// AppProvider endi kerak emas, chunki Zustand global holatni o'zi boshqaradi.
+// React.StrictMode'ni ham olib tashladik, chunki u TWA bilan muammo keltirib chiqarayotgan edi.
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppProvider> {/* Butun ilovani provayder bilan o'raymiz */}
-      <App />
-    </AppProvider>
-  </React.StrictMode>,
+  <App />
 )
