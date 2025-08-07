898f5bf92591caaaa85d6519104fa81b26b5d68d
ABC Developers <info@abclegacyllc.com>
2025-08-07 02:35:06 -0400

    Update main.jsx

--- twa-frontend/src/main.jsx
+++ twa-frontend/src/main.jsx
@@ -4,6 +4,23 @@ import App from './App.jsx'
 import './index.css'
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme.js';
+import * as Sentry from "@sentry/react"; // 1. Sentry'ni import qilamiz
+
+// 2. Sentry'ni sizning DSN kalitingiz bilan ishga tushiramiz
+Sentry.init({
+  dsn: "https://1c970d384ec6c7f7f6ec0d9930801d250a4509801364324352.ingest.us.sentry.io/4509801366290432",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, 
+  // Session Replay
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
