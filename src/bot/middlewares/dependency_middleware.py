9ea1ca8e95a5546f26dd3f72d22d8c8eddc38213
ABC Developers <info@abclegacyllc.com>
2025-08-07 06:01:19 -0400

    Update dependency_middleware.py

--- src/bot/middlewares/dependency_middleware.py
+++ src/bot/middlewares/dependency_middleware.py
@@ -2,40 +2,30 @@ from typing import Callable, Dict, Any, Awaitable
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
 
-from src.bot.services.guard_service import GuardService
-from src.bot.services.scheduler_service import SchedulerService
-from src.bot.services.analytics_service import AnalyticsService
-from src.bot.services.subscription_service import SubscriptionService # <-- Added
+# Loyihadagi kerakli modullarni import qilamiz
 from src.bot.database.repositories import (
     UserRepository,
     ChannelRepository,
-    SchedulerRepository,
-    AnalyticsRepository,
-    PlanRepository # <-- Added
+    PlanRepository,
 )
+from src.bot.services.guard_service import GuardService
+from src.bot.services.subscription_service import SubscriptionService
 
 class DependencyMiddleware(BaseMiddleware):
+    # --- YECHIM: Endi faqat botga kerakli argumentlarni qabul qilamiz ---
     def __init__(
         self,
         user_repo: UserRepository,
         channel_repo: ChannelRepository,
-        scheduler_repo: SchedulerRepository,
-        analytics_repo: AnalyticsRepository,
-        plan_repo: PlanRepository, # <-- Added
+        plan_repo: PlanRepository,
         guard_service: GuardService,
-        scheduler_service: SchedulerService,
-        analytics_service: AnalyticsService,
-        subscription_service: SubscriptionService, # <-- Added
+        subscription_service: SubscriptionService,
     ):
         self.user_repo = user_repo
         self.channel_repo = channel_repo
-        self.scheduler_repo = scheduler_repo
-        self.analytics_repo = analytics_repo
-        self.plan_repo = plan_repo # <-- Added
+        self.plan_repo = plan_repo
         self.guard_service = guard_service
-        self.scheduler_service = scheduler_service
-        self.analytics_service = analytics_service
-        self.subscription_service = subscription_service # <-- Added
+        self.subscription_service = subscription_service
 
     async def __call__(
         self,
@@ -43,14 +33,11 @@ class DependencyMiddleware(BaseMiddleware):
         event: TelegramObject,
         data: Dict[str, Any],
     ) -> Any:
+        # Har bir handler'ga kerakli obyektlarni "data" lug'ati orqali uzatamiz
         data["user_repo"] = self.user_repo
         data["channel_repo"] = self.channel_repo
-        data["scheduler_repo"] = self.scheduler_repo
-        data["analytics_repo"] = self.analytics_repo
-        data["plan_repo"] = self.plan_repo # <-- Added
+        data["plan_repo"] = self.plan_repo
         data["guard_service"] = self.guard_service
-        data["scheduler_service"] = self.scheduler_service
-        data["analytics_service"] = self.analytics_service
-        data["subscription_service"] = self.subscription_service # <-- Added
+        data["subscription_service"] = self.subscription_service
         
         return await handler(event, data)
