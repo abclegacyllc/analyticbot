2e88742be296cba5fce8d55512033bbbed54482d
ABC Developers <info@abclegacyllc.com>
2025-08-10 13:21:58 +0000

    Approved

--- src/bot/container.py
+++ src/bot/container.py
@@ -1,5 +1,8 @@
 import punq
-from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+from aiogram import Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from src.bot.config import load_config, Config
 from src.bot.database.db import create_async_pool
@@ -17,20 +20,32 @@ from src.bot.services import (
     AnalyticsService,
 )
 
-# Bu funksiya konteynerni yaratadi va barcha kerakli
-# obyektlarni (qaramliklarni) ro'yxatdan o'tkazadi.
 def get_container() -> punq.Container:
+    """
+    DI konteynerini yaratadi va barcha kerakli
+    obyektlarni (qaramliklarni) ro'yxatdan o'tkazadi.
+    """
     config = load_config()
     pool = create_async_pool(config.db.construct_connection_url())
 
     container = punq.Container()
+
+    # --- 1-TUZATISH START ---
+    # Bot obyektini "singleton" sifatida ro'yxatdan o'tkazamiz.
+    # Bu shuni anglatadiki, butun ilova davomida yagona Bot obyekti ishlatiladi.
+    @container.register(Bot, scope=punq.Scope.singleton)
+    def get_bot_instance(config: Config) -> Bot:
+        return Bot(
+            token=config.bot.token.get_secret_value(),
+            default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+        )
+    # --- 1-TUZATISH TUGADI ---
 
     # Asosiy resurslarni registratsiya qilamiz
     container.register(Config, instance=config)
     container.register(async_sessionmaker, instance=pool)
 
     # Repozitoriy'larni registratsiya qilamiz
-    # Ular `async_sessionmaker`'ga bog'liqligi avtomatik aniqlanadi.
     container.register(UserRepository)
     container.register(PlanRepository)
     container.register(ChannelRepository)
@@ -38,7 +53,7 @@ def get_container() -> punq.Container:
     container.register(AnalyticsRepository)
 
     # Servis'larni registratsiya qilamiz
-    # Ularning repozitoriy'larga bog'liqligi ham avtomatik aniqlanadi.
+    # Endi ularning Bot obyektiga bog'liqligi avtomatik ta'minlanadi.
     container.register(SubscriptionService)
     container.register(GuardService)
     container.register(SchedulerService)
@@ -47,5 +62,4 @@ def get_container() -> punq.Container:
     return container
 
 # Global konteyner obyektini yaratib qo'yamiz
-# Butun ilova shu bitta obyektni ishlatadi
 container = get_container()
