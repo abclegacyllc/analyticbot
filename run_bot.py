b9f23b41a424804be8c6eb6f11698860518ffa4c
ABC Developers <info@abclegacyllc.com>
2025-08-10 12:09:55 -0400

    Update run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -6,7 +6,7 @@ from aiogram.fsm.storage.redis import RedisStorage
 from aiogram_i18n import I18nMiddleware
 from aiogram_i18n.cores import FluentRuntimeCore
 
-# To'g'ri va bir xil importlardan foydalanamiz
+# Imports are now direct, without 'src'
 from bot.container import container
 from bot.config import settings
 from bot.handlers import admin_handlers, user_handlers
@@ -15,34 +15,28 @@ from bot.utils.language_manager import LanguageManager
 from bot.database.repositories import UserRepository
 
 async def main():
-    """Botni ishga tushiruvchi asosiy funksiya."""
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
-
-    # Tayyor 'settings' obyektini ishlatamiz
     config = settings
 
     if config.SENTRY_DSN:
         sentry_sdk.init(dsn=str(config.SENTRY_DSN), traces_sample_rate=1.0)
-        logger.info("Sentry is configured.")
 
-    # Yagona Bot nusxasini konteynerdan olamiz
     bot: Bot = container.resolve(Bot)
     storage = RedisStorage.from_url(config.REDIS_URL.unicode_string())
     dp = Dispatcher(storage=storage)
 
-    # Middleware'larni sozlash
     user_repo = container.resolve(UserRepository)
     language_manager = LanguageManager(user_repo=user_repo, config=config)
     dp.update.middleware(DependencyMiddleware(container=container))
+
     i18n_middleware = I18nMiddleware(
-        core=FluentRuntimeCore(path="src/bot/locales/{locale}/LC_MESSAGES"),
+        core=FluentRuntimeCore(path="bot/locales/{locale}/LC_MESSAGES"),
         default_locale=config.DEFAULT_LOCALE,
         manager=language_manager
     )
     i18n_middleware.setup(dp)
 
-    # Router'larni ulash
     dp.include_router(admin_handlers.router)
     dp.include_router(user_handlers.router)
 
